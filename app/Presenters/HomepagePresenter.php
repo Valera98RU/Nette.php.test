76f35a8eae7cb13c4dddb58244fc02c7b963e36a
valera98RU@mail.ru <ROVERsaber02021998!@>
2021-03-11 15:46:55 +0700

    добавление пагинатора к странице работников

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -10,19 +10,26 @@ use Nette;
  class HomepagePresenter extends Nette\Application\UI\Presenter
  {
 	private     $database;
+	public $paginator;
 
 	public function __construct(Nette\Database\Explorer $database)
 	{
 
 		$this->database = $database;
+		$this->paginator = new Nette\Utils\Paginator;
 	}
 
-	public function renderDefault(): void
+	public function renderDefault(int $page = 1): void
 	{
-		
+
+        $this->paginator->setPage($page); // the number of the current page (numbered from 1)
+        $this->paginator->setItemsPerPage(10); // the number of records per page
+        $this->paginator->setItemCount($this->database->table('employee')->count());
 		$this->template->employee = $this->database->table('employee')
-			->order('name')
-			->limit(20);
+            ->page($this->paginator->getPage(),$this->paginator->getLength());
+
+
+		$this->template->paginator = $this->paginator;
 
 	}
 
